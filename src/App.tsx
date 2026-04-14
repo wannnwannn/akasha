c995655d672a0b3a015d2a2f99ba4d07598e5d69
wannnwannn <etanefidji@gmail.com>
2026-04-14 16:34:35 +0200

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1146,7 +1146,7 @@ const DiscoverySearch: React.FC<{
   const [community, setCommunity] = useState<LibraryItem[]>([]);
   const [loadingFeeds, setLoadingFeeds] = useState(true);
 
-  const [showManualAdd, setShowManualAdd] = useState(false);
+//  const [showManualAdd, setShowManualAdd] = useState(false);
 
   useEffect(() => {
     if (debouncedQuery) return;
